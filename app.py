79d2df0700dfed33f281bc84642e3861984151aa
rakterc <51397258+rakterc@users.noreply.github.com>
2019-06-05 14:24:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     """Return homepage."""
 #    json_data = {'Hello': 'World!'}
 #    return jsonify(json_data)
-   return "dear"
+    return "dear"
 
 if __name__ == '__main__':
     app.run()
